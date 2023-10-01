fac6eb06414efdd2e2e0340f86ab6c80482caa8a
HiranyaRanawaka99 <hiranya.ranawaka99@gmail.com>
2023-10-01 13:15:51 +0300

    style: millestone 1

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ if($form_submitted) {
     $special_chars = '?!.,-_+*{}[]£$%&/()=';
 
     $allowed_chars = $numeric_chars . $lc_chars . $up_chars . $special_chars;
+    
     $generated_password = '';
-
     for ($i = 0; $i < $password_length; $i++) {
 
         $min = 0;
@@ -33,16 +33,22 @@ var_dump($generated_password);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Generate password</title>
 </head>
 <body>
     <div class="container mt-5">
         <h1>Password generator</h1>
         <form action="" method="GET">
-            <label for="password" class="password">Choose the length of your password</label>
-            <input type="number" name="password" min="3" max="20" value="<?php $password_length ?>">
-            <button class="btn btn-primary">Genera password</button>
-            <div class="generated-password">Your secure password is:</div>
+            <div class="generate-password">
+                <label for="password" class="password">Choose the length of your password</label>
+                <input type="number" name="password" min="3" max="20" value="<?php $password_length ?>">
+                <br>
+                <button class="btn btn-primary">Genera password</button>
+            </div>
+            <hr>
+            <div class="generated-password">Your secure password is: <?= $generated_password?></div>
 
         </form>
     </div>
